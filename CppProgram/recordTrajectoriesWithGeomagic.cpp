2bc70c72e727614d660affbb2aa366638ec99917
DERMY Oriane <oriane.dermy@inria.fr>
2017-02-24 10:14:50 +0100

    add Forces information and time in the recordTrajectories C++ program

--- CppProgram/recordTrajectoriesWithGeomagic.cpp
+++ CppProgram/recordTrajectoriesWithGeomagic.cpp
@@ -1,12 +1,13 @@
 /* 
  * For information: oriane.dermy@inria.fr (11/07/16)
  * 
- * This program is based on the main.cpp program which is an example of the utilization of the arm robot "geomagic Touch".
- * Here, the simulated robot follows the trajectory given by the partner on the geomagic touch arm robot.
+ * In this program, we can control the arm robot the simulated robot using the geomagic touch.
  * During all the movement the partner has to maintain the black button, to allow the robot to record data in the file text.txt. 
- * After that, if we press the white button, the robot will replay the movement. The geomagic touch arm robot will try to follow this movement.
+ * After that, if we press the white button, the robot will replay the movement. The geomagic touch arm robot will follow this movement.
  * 
- * Moreother the orientation of the arm is corrected to be natural.
+ * Be careful, when you replay the trajectory, the geomagic movement is quite brutal, you should keep holding (in a compliant way) the pen of the geomagic to avoid big movement. 
+ * 
+ * Moreover the orientation of the arm is corrected to be natural.
  * 
  */
 
@@ -16,6 +17,7 @@
 #include <map>
 #include <iostream>
 #include <fstream>
+#include <boost/lexical_cast.hpp>
 
 #include <yarp/os/all.h>
 #include <yarp/dev/all.h>
@@ -41,7 +43,7 @@ using namespace hapticdevice;
 class Test: public yarp::os::RFModule
 {
 protected:
-    // geomagic  
+    // geomagic parameters
     PolyDriver drvGeomagic;
     IHapticDevice *igeo;    
     bool fakehaptic;
@@ -51,19 +53,48 @@ protected:
     double maxForce;
     int verbosity;
 
-    // cartesian
+    // cartesian parameters
     CartesianClient client;
     Vector xd;
     Vector od;
     string part;
     
-    //my data
-    vector <vector <double> > fx, fy, fz; //Will be possible to learn other trajectory
-    vector <double>  ftx, fty, ftz; //temporary vector
-    int nbReplay;
+    // my data
+    
+    vector <vector <double> > secTrajX, secTrajY, secTrajZ, secTime; 
+    vector <double>  trajX, trajy, trajz, trajTime; 
+    int nbReplay, totalTraj;
 	int nbTimeStep;
     int flagReplay, flagLearn;
     ofstream record;
+    BufferedPort<Bottle> port;
+    double currentTime;
+    string fileName;
+        
+    bool initFile()
+    {
+		fileName = string("record"+ boost::lexical_cast<string>(totalTraj) + ".txt");  
+		record.open(fileName.c_str(), ios::out | ios::trunc); 
+        if (!record)
+        {  
+			cerr << "Cannot creat the file " << totalTraj  << " for recording movements." << endl;
+			return false;
+		}else
+		{
+			cout << "Correctly created " << fileName << endl;
+		}
+		return true;
+	}
+	bool closeFile()
+	{
+			totalTraj++;
+			record.close();
+			cout << "in close file" << endl;
+			return true;
+	}
+
+
+
 
 
     bool initGeomagic(const string &name, const string &geomagic)
@@ -71,8 +102,7 @@ protected:
         Property optGeo("(device hapticdeviceclient)");
         optGeo.put("remote",("/"+geomagic).c_str());
         optGeo.put("local",("/"+name+"/geomagic").c_str());
-        if (!drvGeomagic.open(optGeo))
-            return false;
+        if (!drvGeomagic.open(optGeo)) return false;
         drvGeomagic.view(igeo);              
 
         Matrix T=yarp::math::zeros(4,4);
@@ -110,9 +140,12 @@ protected:
 
 public:
  
-    Test(int verbositylevel=2): RFModule()
+    Test(Network &yarp, int verbositylevel=2): RFModule()
     {
 
+       port.open("/ori_record/read");  
+
+	   yarp.connect("/wholeBodyDynamicsTree/left_arm/cartesianEndEffectorWrench:o","/ori_record/read", "tcp");//, false); 
     }
     
     bool configure(ResourceFinder &rf)
@@ -133,9 +166,10 @@ public:
         flagReplay = 0;
         flagLearn = 0;
         nbReplay = 0;
-		record.open("record.txt", ios::out | ios::trunc); 
-        if (!record)  cerr << "Cannot creat the file for recording movements." << endl;
+        totalTraj = 0;
+
 	
+		// if we don't want to use the geomagic: not tested here
         if (fakehaptic)
         {
           igeo=new FakeGeomagicDriver();
@@ -149,18 +183,16 @@ public:
         }
         else
         {  
-        if (!initGeomagic(name,geomagic))
-          return false;
+			if (!initGeomagic(name,geomagic)) return false;
         }
-        if (!initCartesian(robot,part,1,1))
-          return false;
+        
+        if (!initCartesian(robot,part,1,1)) return false;
         return true;
     }
     
     bool close()
     {
 		cout << "Closing all modules" << endl;
-		record.close();
 		if (!fakehaptic)
 		{
 			closeGeomagic();
@@ -207,29 +239,33 @@ public:
         if((buttons[0] != 0.0) && (flagLearn ==0))
         {
 			flagLearn = 1;
-			cout << "Replay one from " << fx.size() << " trajectories" << endl;
+			cout << "Replay one from " << secTrajX.size() << " trajectories" << endl;
+			initFile();
 		}
-		else if((buttons[0] == 0.0) && (flagLearn == 1) && (ftx.size() > 2))
+		else if((buttons[0] == 0.0) && (flagLearn == 1) && (trajX.size() > 2))
 		{
 			cout << "End of the recorded trajectory." << endl;
-			fx.push_back(ftx);
-			fy.push_back(fty);
-			fz.push_back(ftz);
-			cout << "We've learned " << fx.size() << " trajectories." << endl;
+			secTrajX.push_back(trajX);
+			secTrajY.push_back(trajy);
+			secTrajZ.push_back(trajz);
+			secTime.push_back(trajTime);
+			cout << "We've learned " << secTrajX.size() << " trajectories." << endl;
 
 			record << endl;
-			ftx.clear();
-			fty.clear();
-			ftz.clear(); 
+			closeFile();
+			trajX.clear();
+			trajy.clear();
+			trajz.clear(); 
+			trajTime.clear();
 			flagLearn = 0;
 		}
 		if((buttons[1]!=0) && (flagReplay==0))
 		{
-			if (fx.size() > 0)
+			if (secTrajX.size() > 0)
 			{
-				cout << "Give the number of the trajectory you want to replay between 0 and " << fx.size() -1 << endl;
+				cout << "Give the number of the trajectory you want to replay between 0 and " << secTrajX.size() -1 << endl;
 				cin >> nbReplay;
-				if( (nbReplay < 0) || nbReplay > fx.size() - 1)
+				if( (nbReplay < 0) || nbReplay > secTrajX.size() - 1)
 				{
 					cout << "I don't understand your answer, so I replay the first one." << endl;
 					nbReplay = 0;
@@ -244,12 +280,22 @@ public:
 		//if we learn
 		if(flagLearn == 1)
 		{
-			ftx.push_back(x[0]);
-			fty.push_back(x[1]);
-			ftz.push_back(x[2]);
-			cout << "Learn = " << ftx.size() << endl;
-			record << x[0] << " " << x[1] << " " << x[2] << endl;
-			
+			/*If you don't want to record forces delet these 6 lines*/
+			Bottle *input = port.read();
+			if (input!=NULL) 
+			{
+				if(verbosity == 1) cout << "Got: " << input->toString().c_str() << endl;
+			}
+			else cout << "Got nothing" << endl;
+		
+			currentTime = Time::now();
+			trajTime.push_back(currentTime);
+			trajX.push_back(x[0]);
+			trajy.push_back(x[1]);
+			trajz.push_back(x[2]);
+			cout << "Learn = " << trajX.size() << endl;
+			record << currentTime << " " << x[0] << " " << x[1] << " " << x[2]  << " " << input->get(0).asDouble() << " " << input->get(1).asDouble() << " " << input->get(2).asDouble() << endl;
+			cout << "record ..." << endl;
 			client.goToPose(xd,od); // new target is xd,od
 			//feedback=yarp::math::operator*(yarp::math::operator-(x,xd), 45.0);	
 			 feedback=yarp::math::operator*(x,0.0);	
@@ -261,9 +307,9 @@ public:
 		{
 			cout << "Replay. Time step = " << nbTimeStep << endl;
 
-			xd[0] = fx[nbReplay][nbTimeStep];
-			xd[1] = fy[nbReplay][nbTimeStep];
-			xd[2] = fz[nbReplay][nbTimeStep];	
+			xd[0] = secTrajX[nbReplay][nbTimeStep];
+			xd[1] = secTrajY[nbReplay][nbTimeStep];
+			xd[2] = secTrajZ[nbReplay][nbTimeStep];	
 			nbTimeStep++;
 			
 			// the sync don't work with icubSim
@@ -275,7 +321,7 @@ public:
 			while(distance > 0.001)
 			{
 				cout << "Distance =" << distance << endl;
-			    yInfo("Sim position    = (%s)",x.toString(3,3).c_str());
+				yInfo("Sim position    = (%s)",x.toString(3,3).c_str());
 				yInfo("Haptic position = (%s)", pos.toString(3,3).c_str());
 				feedback=yarp::math::operator*(yarp::math::operator-(x,pos), 45.0); //(x-xd)*45.0; 
 
@@ -289,7 +335,7 @@ public:
 				client.getPose(x,o);
 				distance= ((x[0] - pos[0])*(x[0] - pos[0]) + (x[1] - pos[1])*(x[1] - pos[1]) + (x[2] - pos[2])*(x[2] - pos[2]));
 			}
-			if (nbTimeStep >= fx[0].size())
+			if (nbTimeStep >= secTrajX[0].size())
 			{
 				cout << "End of the trajectory." << endl;
 				nbTimeStep = 0;
@@ -302,6 +348,47 @@ public:
 			feedback=yarp::math::operator*(x,0.0);	
 			verifyFeedback();
 			igeo->setFeedback(feedback);
+		/** if we want to force the icub and the geomagic to take the origin position do:.
+		 * //we force the icub to take the origin position 
+		*		//client.goToPose(xd,od); // new target is xd,od
+		*		//feedback=yarp::math::operator*(x,0.0);	
+		*		//verifyFeedback();
+		*	    //igeo->setFeedback(feedback);
+		*		xd[0] = -0.04939;
+		*		xd[1] = -0.06839;
+		*		xd[2] = -0.05287;
+		*		double distance;
+		*		do
+		*		{
+		*			client.goToPose(xd,od); // new target is xd,od
+		*			client.getPose(x,o);
+		*			distance= ((x[0] - xd[0])*(x[0] - xd[0]) + (x[1] - xd[1])*(x[1] - xd[1]) + (x[2] - xd[2])*(x[2] - xd[2]));
+		*		}while(distance > 0.001);
+		*		
+		*		//we force the geom to take the origin position
+		*		igeo->getPosition(pos);
+		*		distance= ((xd[0] - pos[0])*(xd[0] - pos[0]) + (xd[1] - pos[1])*(xd[1] - pos[1]) + (xd[2] - pos[2])*(xd[2] - pos[2]));
+		*		//cout << "distance init= " << distance << " x= " << x.toString(3,3).c_str() << "pos=" <<  pos.toString(3,3).c_str()<< endl;
+		*		while(distance > 0.00005)
+		*		{
+		*			cout << "positionning the geomagic... =" << distance << endl;
+		*		    //yInfo("Sim position    = (%s)",x.toString(3,3).c_str());
+		*			//yInfo("Haptic position = (%s)", pos.toString(3,3).c_str()); 
+		*			feedback=yarp::math::operator*(yarp::math::operator-(xd,pos), 50.0); //(x-xd)*45.0; 
+		*
+		*			//yInfo("Feedback = (%s)", feedback.toString(3,3).c_str());
+		*			verifyFeedback(); 
+		*			//yInfo("Feedback = (%s)", feedback.toString(3,3).c_str());
+	    *
+		*			igeo->setFeedback(feedback);			
+		*			igeo->getPosition(pos);
+		*			client.getPose(x,o);
+		*			distance= ((xd[0] - pos[0])*(xd[0] - pos[0]) + (xd[1] - pos[1])*(xd[1] - pos[1]) + (xd[2] - pos[2])*(xd[2] - pos[2]));		
+		*		}
+		*		cout << "Geomagic positionned" << endl;
+		***/
+		
+				
 		}
 		     
         return true;
@@ -323,7 +410,7 @@ int main(int argc,char *argv[])
     ResourceFinder rf;
     rf.configure(argc,argv);
 
-    Test test;    
+    Test test(yarp);    
     int r=test.runModule(rf);  
     
     return r;
